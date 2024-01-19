7ee03daee9599250757aee4ddf0f05be5ac6fde1
alesluin <alesluin@GCFRPA041>
2024-01-19 15:02:10 +0100

    patch ex3

--- StudentLibrary/Solution3.cpp
+++ StudentLibrary/Solution3.cpp
@@ -6,6 +6,17 @@
 #include "StudentConfiguration.h"
 #ifdef COMPILE_EXERCICE_3
 
+class Solution3 {
+public:
+	void SetWords(const std::vector<std::string>& _words);
+	void SortWords();
+	std::vector<std::string> GetSortedWords() const;
+
+private:
+	static bool CaseInsensitiveCompare(const std::string& a, const std::string& b);
+	std::vector<std::string> words;
+};
+
 void Solution3::SetWords(const std::vector<std::string>& _words)
 {
 	words = _words;
@@ -16,13 +27,19 @@ void Solution3::SortWords()
 	if (words.empty()) {
 		throw std::runtime_error("La liste est vide. Impossible de la trier.");
 	}
-	std::sort(words.begin(), words.end());
+	std::sort(words.begin(), words.end(), CaseInsensitiveCompare);
 
 }
 
 std::vector<std::string> Solution3::GetSortedWords() const
 {
 	return words;
+}
+
+bool Solution3::CaseInsensitiveCompare(const std::string& a, const std::string& b)
+{
+	return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end(),
+		[](char c1, char c2) { return std::toupper(c1) < std::toupper(c2); });
 }
 
 #endif
